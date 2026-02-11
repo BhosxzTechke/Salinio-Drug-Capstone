d4b4457eb704bdc39d97951493401407110f078e
BhosxzTechke <danmichaelantiquina9@gmail.com>
2026-02-11 16:40:53 +0800

    debug backup

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -634,7 +634,7 @@ public function UpdateAdmin(Request $request)
                 // }
 
 
-                
+
                 public function BackupDatabase()
                         {
                             $files = Storage::disk('backups')->files(); // list all backups
@@ -658,7 +658,7 @@ public function UpdateAdmin(Request $request)
                 {
                     try {
                         Artisan::call('backup:run');
-                        // dd(Artisan::output());
+                        dd(Artisan::output());
 
                         
                             $notification = array(
@@ -670,7 +670,7 @@ public function UpdateAdmin(Request $request)
 
 
                     } catch (\Throwable $e) {
-                        // dd($e->getMessage());
+                        dd($e->getMessage());
                     }
                 }
 
